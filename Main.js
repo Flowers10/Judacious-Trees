ecbeae2773cb56295b357aeac5d11ec76aa02f7b
Flowers10 <31529269+Flowers10@users.noreply.github.com>
2018-09-26 18:17:48 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,10 +1,10 @@
 //Holds most of the functions for things like buttons
-let HELLOS = 0;
+//let HELLOS = 0;
 
 function test() {
   HELLOS++;
 }
 
 window.setInterval(function() {
-  html.getElementById('test').innerHTML = HELLOS;
+  document.getElementById("test").innerHTML = HELLOS;
 },100)
